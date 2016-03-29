a45e8e9723abbce864432a6789edde0b5eae3d7b
Jaime <jaivalz@yahoo.com>
2016-03-29 15:51:33 +0200

    Added Repolist and repo components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -22,7 +22,6 @@ class App extends Component{
       cache: false,
       success: function(data){
         this.setState({userData: data});
-        console.log(data);
       }.bind(this),
       error: function(xhr, status, err){
         this.setState({username: null});
@@ -31,14 +30,32 @@ class App extends Component{
     });
   }
 
+  // Get user repos
+  getUserRepos(){
+    $.ajax({
+      url: 'https://api.github.com/users/'+this.state.username+'/repos?per_page='+this.state.perPage+'&client_id='+this.props.clientId+'&client_secret='+this.props.clientSecret+'&sorted=created',
+      dataType: 'json',
+      cache: false,
+      success: function(data){
+        this.setState({userRepos: data});
+      }.bind(this),
+      error: function(xhr, status, err){
+        this.setState({username: null});
+        alert(err);
+      }.bind(this)
+    });
+  }
+
+
   componentDidMount(){
     this.getUserData();
+    this.getUserRepos();
   }
 
   render(){
     return(
       <div>
-        <Profile userData = {this.state.userData} />
+        <Profile {...this.state} />
       </div>
     )
   }
